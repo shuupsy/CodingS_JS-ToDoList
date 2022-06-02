59aa60f35ebdf070f754aaf615a4b77e3528820e
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-06-02 12:55:00 +0200

    JS : Batch création

--- public/js/main.js
+++ public/js/main.js
@@ -7,13 +7,47 @@ let liste = document.getElementById("liste")
 // Tâche à faire
 let tache = document.querySelector("input")
 
-newTache.addEventListener("click", function() {
+
+
+newTache.addEventListener("click", () => {
+    // Batch création
     let ligne = document.createElement("div")
+
+    let texte = document.createElement("p")
+    let icones = document.createElement("div")
+
+    let btn1 = document.createElement("button")
+    let btn2 = document.createElement("button")
+    let btn3 = document.createElement("button")
+
+    let icone1 = document.createElement("i")
+    let icone2 = document.createElement("i")
+    let icone3 = document.createElement("i")
+
+    // Appendchild
+    btn1.appendChild(icone1)
+    btn2.appendChild(icone2)
+    btn3.appendChild(icone3)
+
+    icones.appendChild(btn1)
+    icones.appendChild(btn2)
+    icones.appendChild(btn3)
+
+    ligne.appendChild(texte)
+    ligne.appendChild(icones)
+
     liste.appendChild(ligne)
-    ligne.innerHTML = tache.value;
+  
+    // Valeur du input donnée au nom de la tâche
+    texte.innerHTML = tache.value;
     // Supprimer la valeur du input
     tache.value = ""
 
+    // Attribuer les class CSS 
     ligne.setAttribute("class", "ligne")
-
+    icones.setAttribute("class", "icones")
+    icone1.setAttribute("class", "fa-solid fa-circle-check")
+    icone2.setAttribute("class", "fa-solid fa-pen-to-square")
+    icone3.setAttribute("class", "fa-solid fa-trash-can")
 })
+
